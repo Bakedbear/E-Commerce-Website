7c0e1462529afd944ba2c41a33cd329fe8a0c742
RennieKoske <Bakedbear78194@gmail.com>
2023-11-22 13:44:22 +0300

    added cart page, product detail page,checkout page added offer page

--- script.js
+++ script.js
@@ -56,3 +56,79 @@ searchButton.addEventListener('click', function() {
 tClose.addEventListener('click', function() {
     showClass.classList.remove('showsearch')
 })
+
+
+//show dpt menu
+const dptButton = document.querySelector('.dpt-cat .dpt-trigger'),
+    dptClass = document.querySelector('.site');
+dptButton.addEventListener('click', function() {
+    dptClass.classList.toggle('showdpt')
+})
+
+
+//product image slider
+var productThumb = new Swiper ('.small-image', {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        481: {
+            spaceBetween: 32,
+        }
+    }
+});
+
+var productBig = new Swiper ('.big-image', {
+    loop: true,
+    autoHeight: true,
+    navigation: {
+        nexEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: productThumb,
+    }
+})
+
+//stock products var width percentage
+var stocks = document.querySelectorAll('.products .stock');
+for (let x = 0; x < stocks.length; x++) {
+    let stock = stocks[x].dataset.stock,
+    available = stocks[x].querySelector('.qty-available').innerHTML,
+    sold = stocks[x].querySelector('.qty-sold').innerHTML,
+    percent = sold*100/stock;
+
+    stocks[x].querySelector('.available').style.width = percent + '%';
+}
+
+//show cart on click
+const divtoShow = '.mini-cart';
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector('.cart-trigger');
+
+divTrigger.addEventListener('click', () => {
+    setTimeout(() => {
+        if(!divPopup.classList.contains('show')) {
+            divPopup.classList.add('show');
+        }
+    }, 250 )
+})
+
+//close by click outside
+document.addEventListener('click', (e) => {
+    const isClosest = e.target.closest(divtoShow);
+    if(!isClosest && divPopup.classList.contains('show')) {
+        divPopup.classList.remove('show');
+    }
+})
+
+
+//show modal on load
+window.onload = function () {
+    document.querySelector('.site').classList.toggle('showmodal')
+}
+document.querySelector('.modalclose').addEventListener('click', function() {
+    document.querySelector('.site').classList.remove('showmodal')
+})
